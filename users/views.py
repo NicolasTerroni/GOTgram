466744bbc5279a93b5229f3422c2d03cf7e93526
nicolasterroni <nsterroni@gmail.com>
2020-10-28 23:42:22 -0300

    urls for each app, user profile with class-based view

--- users/views.py
+++ users/views.py
@@ -30,7 +30,7 @@ def update_profile(request):
 
             messages.success(request, 'Your profile has been updated!')
 
-            return redirect('update_profile')
+            return redirect('users:update_profile')
     else:
         form = ProfileForm()
     return render(
@@ -52,7 +52,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('feed')
+            return redirect('posts:feed')
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
@@ -65,7 +65,7 @@ def signup_view(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("login")
+            return redirect("users:login")
     else:
         form = SignupForm()
     
@@ -81,4 +81,4 @@ def signup_view(request):
 def logout_view(request):
     """Logout a user."""
     logout(request)
-    return redirect("login")
+    return redirect("users:login")
